7edfd3a4c9c5823bff7fa4e8ff36413ba7c453ac
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-02-25 09:03:34 +0530

    questions solved

---
+++ 000 - questions/find_power.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int ans = 1;
+
+    for(int i = 0; i <=30; i++){
+
+        ans = ans * 2;
+        cout << ans << endl;
+
+    }
+
+
+}
